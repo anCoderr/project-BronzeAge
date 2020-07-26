1aa9c0afbac963cd0dbf65b7383f28acf503fc56
anCoderr <deyol.anurag998@gmail.com>
2020-07-26 23:50:59 +0530

    Just Created

--- Fav List App/app/src/main/java/com/example/favlistapp/CategoryRecyclerViewAdapter.java
+++ Fav List App/app/src/main/java/com/example/favlistapp/CategoryRecyclerViewAdapter.java
@@ -1,7 +1,34 @@
 package com.example.favlistapp;
 
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class CategoryRecyclerViewAdapter extends RecyclerView.Adapter<> {
+public class CategoryRecyclerViewAdapter extends RecyclerView.Adapter<CategoryViewHolder> {
 
+    String[] categories = {"Hobbies", "Sports", "Games", "Gadgets", "Food", "Countries"};
+
+    @NonNull
+    @Override
+    public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.category_viewholder, parent, false);
+        return new CategoryViewHolder(view);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onBindViewHolder(@NonNull CategoryViewHolder holder, int position) {
+        holder.getTxtCategoryNumber().setText(Integer.toString(position + 1));
+        holder.getTxtCategoryName().setText(categories[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return categories.length;
+    }
 }
--- Fav List App/app/src/main/java/com/example/favlistapp/CategoryViewHolder.java
+++ Fav List App/app/src/main/java/com/example/favlistapp/CategoryViewHolder.java
@@ -1,8 +1,27 @@
 package com.example.favlistapp;
 
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.recyclerview.widget.RecyclerView;
 
 public class CategoryViewHolder extends RecyclerView.ViewHolder {
 
+    private TextView txtCategoryNumber;
+    private TextView txtCategoryName;
+
+    public CategoryViewHolder(View view) {
+        super(view);
+        txtCategoryNumber = view.findViewById(R.id.categoryNumberTextView);
+        txtCategoryName = view.findViewById(R.id.categoryNameTextView);
+    }
+
+    public TextView getTxtCategoryNumber() {
+        return txtCategoryNumber;
+    }
+
+    public TextView getTxtCategoryName() {
+        return txtCategoryName;
+    }
 
 }
