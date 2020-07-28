7164f312db84a6175aaf58f24ff3b7a8db1b02e1
anCoderr <deyol.anurag998@gmail.com>
2020-07-28 15:26:44 +0530

    committing

---
+++ FavListApp/app/src/main/java/com/example/favlistapp/CategoryRecyclerAdapter.java
@@ -0,0 +1,36 @@
+package com.example.favlistapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CategoryRecyclerAdapter extends RecyclerView.Adapter <CategoryViewHolder> {
+
+    String[] categories = {"Hobbies", "Sports", "Games", "Gadgets", "Foods", "Countries"};
+
+    @NonNull
+    @Override
+    public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.category_viewholder, parent, false);
+        return new CategoryViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryViewHolder holder, int position) {
+
+        holder.getTxtCategoryNumber().setText(Integer.toString(position+1)+".");
+        holder.getTxtCategoryName().setText(categories[position]);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return categories.length;
+    }
+}
